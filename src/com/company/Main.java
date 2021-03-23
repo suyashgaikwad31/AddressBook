e065801170893d7ae91072721f2cbe993f1d3da1
unknown <suyashgaikwad31@gmail.com>
2021-03-23 14:13:24 +0530

    added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,14 +39,58 @@ public class Main {
         String condition =  scan.next();
         if (condition.equals("Y")) {
             Contacts();
+        } else {
+            choiceCondition();
         }
     }
+
+    public void editContact() {
+        System.out.print("Edit First Name: ");
+        String name = scan.next();
+        for(int i = 0; i < contact.size(); i++){
+            if(contact.get(i).contains(name)) {
+                System.out.println("Edit First Name ");
+                for(int j = 0; j < contact.get(i).size(); j++) {
+                    System.out.println(j + " " + contact.get(i).get(j));
+                }
+                System.out.println("Edit Last Name");
+                for(int j = 0; j < contact.get(i).size(); j++) {
+                    System.out.println(j + " " + contact.get(i).get(j));
+                }
+                System.out.print("Enter the Number whic do you Want edit ");
+                int info = scan.nextInt();
+                System.out.print("Enter the new data ");
+                String newInfo = scan.next();
+                contact.get(i).set(info, newInfo);
+                System.out.println("Data Changed ");
+                break;
+            }
+        }
+    }
+
+    public void choiceCondition() {
+        System.out.println( + contact.size());
+        System.out.println("Select condition \n 1) Add New Contact\n 2) Edit Existing Contact\n ");
+        int choice = scan.nextInt();
+        switch (choice) {
+            case 1:
+                addcontacts();
+                break;
+            case 2:
+                editContact();
+                break;
+            default:
+                System.out.println("Enter the number");
+                choiceCondition();
+        }
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         Main obj = new Main();
-        obj.Contacts();
-        obj.addcontacts();
+        obj.choiceCondition();
     }
 
 
